af132599e44420796421c1caaa5aec4ec8c5dbae
jinyongsheng1 <924917145@qq.com>
2018-08-06 11:06:59 +0800

    改了search.css

---
+++ 微信/JS/index.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("#title-right").click( function () {
+        $("#title-add-active").toggle();
+        
+    })
+    
+})
+$(function () {
+
+    $("#title-search").click( function () {
+        $("#hidden").toggle();
+
+    })
+    $("#hidden").click( function () {
+        $("#hidden-2").toggle();
+
+    })
+})
+    
+
+    
